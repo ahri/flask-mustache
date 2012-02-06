34d3d665be99b4f3dccfa8dcdccc02ad998183e6
Adam Piper <adam@ahri.net>
2012-02-06 13:58:09 +0000

    Add error/exception handling.

--- flaskext/mustache.py
+++ flaskext/mustache.py
@@ -15,3 +15,26 @@ def viewroute(app, rule):
         return c
 
     return class_wrapper
+
+def viewerror(app, code_or_exception, return_code=None):
+    """Decorator for a pystache error View"""
+
+    if return_code is None:
+        if type(code_or_exception) is int:
+            return_code = code_or_exception
+        else:
+            return_code = 500
+
+    def class_wrapper(c):
+
+        def render(err, **kwargs):
+            o = c()
+            o.err = err
+            o.route_args = kwargs
+            return o.render(), return_code
+
+        app.register_error_handler(code_or_exception, render)
+
+        return c
+
+    return class_wrapper
--- tests/test_mustache.py
+++ tests/test_mustache.py
@@ -6,12 +6,12 @@ from pystache import View
 
 import sys
 sys.path.append("flaskext")
-from mustache import viewroute
+from mustache import viewroute, viewerror
 
 class Decorator(TestCase):
 
     """
-    Test the view decorator
+    Test the view decorator.
     """
 
     def create_app(self):
@@ -22,6 +22,7 @@ class Decorator(TestCase):
 
     def setup_view(self, *args, **kwargs):
         """Create a view and decorate it with our pystache viewroute"""
+
         @viewroute(self.app, *args, **kwargs)
         class Example(View):
 
@@ -74,3 +75,196 @@ class Decorator(TestCase):
 
         # Assert
         self.assertEquals(response.data, "hellodynamic")
+
+class Decorator404(TestCase):
+
+    """
+    Test the view decorator when a 404 occurs.
+    """
+
+    def create_app(self):
+        """Make a test app"""
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        return self.app
+
+    def setup_view(self, *args, **kwargs):
+        """Create a view and decorate it with our pystache viewroute"""
+
+        @viewerror(self.app, 404, *args, **kwargs)
+        class Example404(View):
+
+            """
+            An example 404 view
+            """
+
+            def __init__(self, *args, **kwargs):
+                super(Example404, self).__init__(*args, **kwargs)
+                self.template = "{{hello}}"
+
+            def hello(self):
+                return "hello 404"
+
+    def test_invalid_response(self):
+        """Test that the application gives an invalid response for our incorrect test route"""
+        # Arrange
+        route = '/test404'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assert404(response)
+
+    def test_valid_content_mustache_var(self):
+        """Test that the application gives valid content for a template with a simple mustache var to set"""
+        # Arrange
+        route = '/test404'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assertEquals(response.data, "hello 404")
+
+class DecoratorException(TestCase):
+
+    """
+    Test the view decorator when an exception occurs.
+    """
+
+    def create_app(self):
+        """Make a test app"""
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        return self.app
+
+    def setup_view(self, *args, **kwargs):
+        """Create a view and decorate it with our pystache viewroute"""
+
+        @viewroute(self.app, '/testexception', *args, **kwargs)
+        class ExampleRaiseException(View):
+
+            """
+            An example view
+            """
+
+            def __init__(self, *args, **kwargs):
+                super(ExampleRaiseException, self).__init__(*args, **kwargs)
+                self.template = "{{hello}}"
+
+            def hello(self):
+                raise Exception("testing")
+
+        @viewerror(self.app, Exception, *args, **kwargs)
+        class ExampleException(View):
+
+            """
+            An example Exception view
+            """
+
+            def __init__(self, *args, **kwargs):
+                super(ExampleException, self).__init__(*args, **kwargs)
+                self.template = "{{hello}}{{dynamic}}"
+
+            def hello(self):
+                return "hello exception"
+
+            def dynamic(self):
+                return self.route_args.get('dynamic')
+
+    def test_invalid_response(self):
+        """Test that the application gives a 500 response when an exception is raised"""
+        # Arrange
+        route = '/testexception'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assertStatus(response, 500)
+
+    def test_valid_content_mustache_var(self):
+        """Test that the application renders a template when an exception is raised"""
+        # Arrange
+        route = '/testexception'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assertEquals(response.data, "hello exception")
+
+class DecoratorExCustomCode(TestCase):
+
+    """
+    Test the view decorator when an exception occurs and return a custom code.
+    """
+
+    def create_app(self):
+        """Make a test app"""
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        return self.app
+
+    def setup_view(self, *args, **kwargs):
+        """Create a view and decorate it with our pystache viewroute"""
+
+        @viewroute(self.app, '/testnotimplemented', *args, **kwargs)
+        class ExampleRaiseNotImplemented(View):
+
+            """
+            An example view
+            """
+
+            def __init__(self, *args, **kwargs):
+                super(ExampleRaiseNotImplemented, self).__init__(*args, **kwargs)
+                self.template = "{{hello}}{{dynamic}}"
+
+            def hello(self):
+                raise NotImplementedError("testing")
+
+            def dynamic(self):
+                return self.route_args.get('dynamic')
+
+        @viewerror(self.app, NotImplementedError, 501, *args, **kwargs)
+        class ExampleCustomCode(View):
+
+            """
+            An example Exception view
+            """
+
+            def __init__(self, *args, **kwargs):
+                super(ExampleCustomCode, self).__init__(*args, **kwargs)
+                self.template = "{{hello}}"
+
+            def hello(self):
+                return "hello custom code"
+
+    def test_invalid_response(self):
+        """Test that the application gives a 501 response when an exception is raised"""
+        # Arrange
+        route = '/testnotimplemented'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assertStatus(response, 501)
+
+    def test_valid_content_mustache_var(self):
+        """Test that the application renders a template when an exception is raised"""
+        # Arrange
+        route = '/testnotimplemented'
+        self.setup_view()
+
+        # Act
+        response = self.client.get(route)
+
+        # Assert
+        self.assertEquals(response.data, "hello custom code")
