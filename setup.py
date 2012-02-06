fb1dc171144ded1d8fc664ceb82ac823bd6ecba2
Adam Piper <adam@ahri.net>
2012-02-06 14:00:21 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Mustache',
-    version='0.1.1',
+    version='0.2.0',
     url='http://github.com/ahri/flask-mustache',
     license='AGPLv3',
     author='Adam Piper',
